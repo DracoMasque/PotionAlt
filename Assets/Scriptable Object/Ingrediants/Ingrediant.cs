1989a61ffbe832d5f4961cc3323a932248ed9cf1
aksel <aksel.boulongne@gmail.com>
2026-09-15 14:47:57 +0200

    début code client

---
+++ Assets/Scriptable Object/Ingrediants/Ingrediant.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ingrediant", menuName = "Scriptable Objects/Ingrediant")]
+public class Ingrediant : ScriptableObject
+{
+    public string Name;
+}
