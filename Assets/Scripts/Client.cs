7834a94d261bfe060b05443fb4743fffa58d9997
aksel <aksel.boulongne@gmail.com>
2026-09-17 12:42:42 +0200

    client réparé

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -101,6 +101,7 @@ public class Client : MonoBehaviour
     private void FacePlayer()
     {
         clientSprite.sprite = spriteBase;
+        ChangeSprite(spriteBase);
         animation.Play("Donne Commande");
     }
     
