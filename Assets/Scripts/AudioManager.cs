4188f264c43e8d20945cd45b2875942a3e85698c
clem <clementine.noemie.choquet@gmail.com>
2026-09-21 12:09:35 +0200

    idk

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+//  djeydki"_èaé&zhyutrkoiçe'_"dofr"'ièhu" +
+//   "" +
+//   normazleme,t je peux rire sza,d trgztfdrt yu boid <;
+//:   en gros ce que j'esszire fr yr fitr 
+// a pztrtir dfu momr,ny ou jr tzyr un,dofr'
+                
+// print( de mon esprit   je pense qu'on a bien plus le temps que ce qu'on se rend cpompte
+//parce que en fait je t'ai déja erxpliqué le théoreme de la bite moulue ?'
+//  en gros je t'explique fermeeee ta gueuuleeeeeee
+//  c'est ^pas ùmpo c'est mmes ùao,s 
+//    c'est mles mùains qui écrivent '*
+//    clemm a l'aode ke sios pprisonnier' );
+
+
+public class AudioManager : MonoBehaviour
+{
+    void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        
+    }
+}
