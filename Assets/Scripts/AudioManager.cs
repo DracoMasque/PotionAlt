a121d0ebe542616eab49fc21bed676f84bf132f9
clem <clementine.noemie.choquet@gmail.com>
2026-09-22 12:55:36 +0200

    Audio Manager complet (normalement)

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -16,13 +16,66 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    void Start()
+    
+    private static AudioManager _instance;
+
+    public static AudioManager Instance
     {
-     
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("AudioManager vide");
+            }
+            return _instance;
+        }
+    }
+    [Header("ClipsAudio")]
+    [SerializeField] private AudioClip[] SfxFichier;
+    [SerializeField] private AudioClip[] MusicFichier;
+
+    [SerializeField] private AudioSource SfxAudioSource;
+    [SerializeField] private AudioSource MusicAudioSource;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    void Update()
+    public void JoueSfx(int valeur)
     {
-        
+        if (valeur < 0 || valeur >= SfxFichier.Length)
+        {
+            SfxAudioSource.Pause();
+            return;
+        }
+        SfxAudioSource.clip =  SfxFichier[valeur];
+        SfxAudioSource.Play();
+    }
+
+    public void JoueSfx(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            SfxAudioSource.clip = clip;
+            SfxAudioSource.Play();
+        }
+    }
+
+    public void JoueMusic(int valeur)
+    {
+        if(valeur < 0 || valeur >= MusicFichier.Length)
+        {
+            MusicAudioSource.Pause();
+            return;
+        }
+        MusicAudioSource.clip = MusicFichier[valeur];
+        MusicAudioSource.Play();
     }
 }
